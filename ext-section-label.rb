c39902def2f7d85801390cfa0921e6553f42dc7e
Masao Takaku <tmasao@acm.org>
2021-09-23 11:51:58 +0900

    add section label extraction. fix #9

--- ext-section-label.rb
+++ ext-section-label.rb
@@ -1,11 +1,38 @@
 #!/usr/bin/env ruby
 
+require "csv"
+require "roo"
+
 def extract_section_label(str)
   result = {}
-  normalized_str = str.tr("　Ａ-Ｚ０-９ｱ-ﾝ", " A-Z0-9ア-ン")
-  if /\A(第\d+章|第\d+節|第\d+|\(\d+\)|\(?[ア-ン]\)?)\s+(.+)\z/ =~ normalized_str
+  normalized_str = str.tr("　Ａ-Ｚ０-９ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄ", " A-Z0-9アイウエオカキクケコサシスセソタチツテト")
+  if /\A(第\d+章|第\d+節|第\d+|\(\d+\)|\d+|\(?[ア-ン]\)?|[A-Za-z]+|[①-⑳])\s+(.+)\z/m =~ normalized_str
     { section: $1, text: $2 }
   else
     { text: normalized_str }
   end
+end
+
+if $0 == __FILE__
+  xlsx = Roo::Excelx.new(ARGV[0], {headers: true})
+  #xlsx.default_sheet = "all_数字はAに"
+  idx = nil
+  idx_text = nil
+  print "\xEF\xBB\xBF" #UTF-8 BOM
+  csv_str = CSV.generate(row_sep: "\r\n") do |csv|
+    xlsx.each_row_streaming(pad_cells: true) do |row|
+      if idx.nil?
+        row.each_with_index do |r, i|
+          idx = i if r and r.value == "学習指導要領コード"
+          idx_text = i if r and r.value == "学習指導要領テキスト"
+        end
+        next
+      end
+      next if row[idx].value.nil?
+      result = extract_section_label(row[idx_text].value)
+      csv << [ row[idx], #row[idx_text],
+               result[:section], result[:text].gsub(/\n/, "\r\n") ]
+    end
+  end
+  print csv_str
 end
--- spec/ext-section-label_spec.rb
+++ spec/ext-section-label_spec.rb
@@ -4,23 +4,49 @@ require "ext-section-label.rb"
 RSpec.describe "extract_section_label" do
   example_file = File.join(File.dirname(__FILE__), "..", "examples","20210820-mxt_syoto01-000010374_01.xlsx")
   it "should work basic functions" do
-    [
-      [
-        "第２章　各教科",
-        "第2章", "各教科"
-      ],
-      [
-        "第１節　国　　　語",
-        "第1節", "国   語"
-      ],
-      [
-        "(1) 日常生活に必要な国語について，その特質を理解し適切に使うことができるようにする。",
-        "(1)", "日常生活に必要な国語について，その特質を理解し適切に使うことができるようにする。"
-      ],
-    ].each do |input, section, text|
-      result = extract_section_label(input)
-      expect(result[:section]).to eq section
-      expect(result[:text]).to eq text
-    end
+    result = extract_section_label("第２章　各教科")
+    expect(result[:section]).to eq "第2章"
+    expect(result[:text]).to eq "各教科"
+
+    result = extract_section_label("第１節　国　　　語")
+    expect(result[:section]).to eq "第1節"
+    expect(result[:text]).to eq "国   語"
+
+    result = extract_section_label("１　教育課程の改善と学校評価等")
+    expect(result[:section]).to eq "1"
+    expect(result[:text]).to eq "教育課程の改善と学校評価等"
+
+    result = extract_section_label("(1) 日常生活に必要な国語について，その特質を理解し適切に使うことができるようにする。")
+    expect(result[:section]).to eq "(1)"
+    expect(result[:text]).to eq "日常生活に必要な国語について，その特質を理解し適切に使うことができるようにする。"
+
+    result = extract_section_label("２　教科等横断的な視点に立った資質・能力の育成")
+    expect(result[:section]).to eq "2"
+    expect(result[:text]).to eq "教科等横断的な視点に立った資質・能力の育成"
+
+    result = extract_section_label("２　教科等横断的な視点に立った資質・能力の育成")
+    expect(result[:section]).to eq "2"
+    expect(result[:text]).to eq "教科等横断的な視点に立った資質・能力の育成"
+
+    result = extract_section_label("(ｳ) 給食，休憩などの時間については，各学校において工夫を加え，適切に定めること。")
+    expect(result[:section]).to eq "(ウ)"
+    expect(result[:text]).to eq "給食，休憩などの時間については，各学校において工夫を加え，適切に定めること。"
+
+    result = extract_section_label("Ｂ　書くこと")
+    expect(result[:section]).to eq "B"
+    expect(result[:text]).to eq "書くこと"
+
+    result = extract_section_label("１　主体的・対話的で深い学びの実現に向けた授業改善\n　各教科等の指導に当たっては，次の事項に配慮するものとする。")
+    expect(result[:section]).to eq "1"
+    expect(result[:text]).to eq "主体的・対話的で深い学びの実現に向けた授業改善\n 各教科等の指導に当たっては，次の事項に配慮するものとする。"
+
+    result = extract_section_label("③　物の溶け方，振り子の運動，電流がつくる磁力について追究する中で，主体的に問題解決しようとする態度を養う。")
+    expect(result[:section]).to eq "③"
+    expect(result[:text]).to eq "物の溶け方，振り子の運動，電流がつくる磁力について追究する中で，主体的に問題解決しようとする態度を養う。"
+
+    result = extract_section_label("a 　単文")
+    expect(result[:section]).to eq "a"
+    expect(result[:text]).to eq "単文"
+
   end
 end
